96a1c22000131da7b4ade7bb61280dfea1eadfac
toshasmlv <t_musirkhan@kbtu.kz>
2026-02-23 11:09:51 +0500

    edited task about json

--- json/1.py
+++ json/1.py
@@ -8,10 +8,13 @@ print("=" * 80)
 print(f"{'DN':<50} {'Description':<20} {'Speed':<6} {'MTU':<6}")
 print("-" * 50 + " " + "-" * 20 + " " + "-" * 6 + " " + "-" * 6)
 
-for intf in data['imdata']:
-    dn = intf['l1PhysIf']['attributes'].get('dn', '')
-    desc = intf['l1PhysIf']['attributes'].get('descr', '')
-    speed = intf['l1PhysIf']['attributes'].get('speed', '')
-    mtu = intf['l1PhysIf']['attributes'].get('mtu', '')
+for item in data.get('imdata', []):
+    phys = item.get('l1PhysIf', {})
+    attr = phys.get('attributes', {})
+    
+    dn = attr.get('dn', '')
+    desc = attr.get('descr', '')
+    speed = attr.get('speed', '')
+    mtu = attr.get('mtu', '')
     
     print(f"{dn:<50} {desc:<20} {speed:<6} {mtu:<6}")
